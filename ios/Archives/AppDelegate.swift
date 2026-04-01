543a9e460bcfdc1e2c5d4499fe23463626c24e46
huyaffinity <huy@affinitylabs.ai>
2026-04-01 08:10:28 +0700

    feat(AFF-634): implement rich push notification on ios

--- ios/Archives/AppDelegate.swift
+++ ios/Archives/AppDelegate.swift
@@ -66,6 +66,11 @@ public class AppDelegate: ExpoAppDelegate {
         }
     }
 
+
+    // AFF-331: Cap Dynamic Type to default size for consistent UI
+    if #available(iOS 15.0, *) {
+      window?.maximumContentSizeCategory = .large
+    }
     return super.application(application, didFinishLaunchingWithOptions: modifiedLaunchOptions)
   }
 
